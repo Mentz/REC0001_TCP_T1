e768255eb98b661dbe19600e08cc1d5c21c17f5c
Mentz <Mentz@users.noreply.github.com>
2017-10-21 11:32:04 -0200

    Update cliente_100ms_full.sh

--- cliente_100ms_full.sh
+++ cliente_100ms_full.sh
@@ -5,38 +5,38 @@ INTERFACE=$2
 DL=100ms
 THR=0
 LOSS=0
-#tc qdisc del dev $INTERFACE root 
-#tc qdisc add dev $INTERFACE root netem delay $DL rate $THR loss $LOSS
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
+tc qdisc del dev $INTERFACE root 
+tc qdisc add dev $INTERFACE root netem delay $DL rate $THR loss $LOSS
+iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
 
 # DELAY 100, THROTTLE 0, LOSS 0.001%
 LOSS=0.001
-#tc qdisc del dev $INTERFACE root 
-#tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
+tc qdisc del dev $INTERFACE root 
+tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
+iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
 
 # DELAY 100, THROTTLE 0, LOSS 0.01%
 LOSS=0.01
-#tc qdisc del dev $INTERFACE root 
-#tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
+tc qdisc del dev $INTERFACE root 
+tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
+iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
 
 # DELAY 100, THROTTLE 0, LOSS 0.1%
 LOSS=0.1
-#tc qdisc del dev $INTERFACE root 
-#tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
-#iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
+tc qdisc del dev $INTERFACE root 
+tc qdisc add dev $INTERFACE root netem delay $DL loss $LOSS
+iperf -c $SERVER_IP -t 120 -i 1 -p 50000 -e -Z reno >> "results/$DL-$THR-$LOSS-reno.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50001 -e -Z cubic >> "results/$DL-$THR-$LOSS-cubic.txt"
+iperf -c $SERVER_IP -t 120 -i 1 -p 50002 -e -Z bbr >> "results/$DL-$THR-$LOSS-bbr.txt"
 iperf -c $SERVER_IP -t 120 -i 1 -p 50003 -e -Z veno >> "results/$DL-$THR-$LOSS-veno.txt"
 
 # DELAY 100, THROTTLE 0, LOSS 1.0%
